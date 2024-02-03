9f2aab4263c659f53dbf365fe4c971b328a5fdf9
scarf <greenscarf005@gmail.com>
2024-02-03 18:49:53 +0900

    refactor: use record

--- dist/roguelike_tag.user.js
+++ dist/roguelike_tag.user.js
@@ -17,6 +17,30 @@
 {
 	"use strict"
 
+	/** @type {<T, O, K extends string>(record: Readonly<Record<K, T>>, fn: (value: T, key: K) => O) => Record<K, O>} */
+	const mapValues = (record, fn) =>
+		// @ts-ignore: using typescript on js is hard
+		Object.fromEntries(Object.entries(record).map(([k, v]) => [k, fn(v)]))
+
+	/** @type {(r: RegExp) => RegExp} */
+	const reTag = (r) => new RegExp(`^(${r.source}\\s*\\)?\\s*)`, "i")
+
+	/** @type {Record<string, { re: RegExp, color: string }>} */
+	const tags = mapValues({
+		돌죽: { re: /(?:돌죽|ㄷㅈ)/, color: "#63200b" },
+		톰죽: { re: /(?:톰죽|ㅌㅈ|tome4?)/, color: "#9227b0" },
+		밝밤: { re: /(?:카타클|ㅋㅌㅋ)?\s*(?:ㅂㅂ|밝밤|bn)/, color: "#2db53d" },
+		dda: { re: /(?:카타클|ㅋㅌㅋ)?\s*(?:어둠밤|dda)/, color: "#1c4f3c" },
+		coq: { re: /(?:coq|caves of qud|qud)/, color: "#6fa698" },
+		엘린: { re: /엘린/, color: "#f5ab7a" },
+		엘로나: { re: /엘로나\+?/, color: "#d6651a" },
+		드포: { re: /(?:ㄷㅍ|드포)/, color: "#f5bf36" },
+	}, ({ re, color }) => ({ re: reTag(re), color }))
+
+	const colors = Object.entries(tags)
+		.map(([key, { color }]) => /*css*/ `[data-label="${key}"] { --label-color: ${color}; }`)
+		.join("\n")
+
 	GM_addStyle(/*css*/ `
         span[data-label] {
             color: white;
@@ -27,49 +51,34 @@
             background-color: var(--label-color);
         }
 
-        [data-label="돌죽"] { --label-color: #63200b; }
-        [data-label="톰죽"] { --label-color: #9227b0; }
-        [data-label="밝밤"] { --label-color: rgb(21, 152, 24); }
-        [data-label="dda"] { --label-color: #1c4f3c; }
-        [data-label="coq"] { --label-color: #6fa698; }
-        [data-label="엘린"] { --label-color: #f5ab7a; }
-        [data-label="엘로나"] { --label-color: #d6651a; }
-        [data-label="드포"] { --label-color: #f5bf36; }
+        ${colors}
     `)
-
-	/** @type {(r: RegExp) => RegExp} */
-	const re = (r) => new RegExp(`^(${r.source}\\s*\\)?\\s*)`, "i")
-
-	const 돌죽 = re(/(?:돌죽|ㄷㅈ)/)
-	const 톰죽 = re(/(?:톰죽|ㅌㅈ|tome4?)/)
-	const 밝밤 = re(/(?:카타클|ㅋㅌㅋ)?\s*(?:ㅂㅂ|밝밤|bn)/)
-	const dda = re(/(?:카타클|ㅋㅌㅋ)?\s*(?:어둠밤|dda)/)
-	const coq = re(/(?:coq|caves of qud|qud)/)
-	const 엘린 = re(/엘린/)
-	const 엘로나 = re(/엘로나\+?/)
-    const 드포 = re(/(?:ㄷㅍ|드포)/)
-
-	const tags = { 돌죽, 톰죽, 밝밤, dda, coq, 엘린, 엘로나, 드포 }
 
 	const tag = () =>
 		Array
-			.from(document.querySelectorAll("tr.us-post td.gall_tit a:first-child:has(em),.subjectin"))
-			.forEach((el) => {
-				const key = Object.entries(tags).find(([, tag]) => tag.test(el.innerText))?.[0]
-				if (!key) return
+			.from(
+				document.querySelectorAll("tr.us-post td.gall_tit a:first-child:has(em),span.subjectin"),
+			)
+			.flatMap((el) => {
+				const search = Object.entries(tags).find(([, { re }]) => re.test(el.innerText))
+				if (!search) return []
 
+				const [key, { re }] = search
+				return [{ el, key, re }]
+			})
+			.forEach(({ el, key, re }) => {
 				el.innerHTML = /*html*/ `
                     <span data-label="${key}">${key}</span>
-                    ${el.innerText.replace(tags[key], "")}
+                    ${el.innerText.replace(re, "")}
                 `
 			})
 
-    tag()
+	tag()
 
 	const tbody = document.querySelector("tbody")
-    if (tbody) {
-	    const observer = new MutationObserver(tag)
+	if (tbody) {
+		const observer = new MutationObserver(tag)
 
-	    observer.observe(tbody, { childList: true })
-    }
+		observer.observe(tbody, { childList: true })
+	}
 }
