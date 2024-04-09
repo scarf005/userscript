2f56b83d855a575cd2204a02bb0484dc098a4ec9
scarf <greenscarf005@gmail.com>
2024-04-09 21:43:36 +0900

    feat: isomorph to typescript

---
+++ packages/isomorph/build.ts
@@ -0,0 +1,30 @@
+import { resolve } from "https://deno.land/std@0.220.1/path/resolve.ts"
+
+import { bundleUserScript } from "../../build.ts"
+import { metadataBlock } from "../../userscript.ts"
+
+const metadata = metadataBlock({
+	entries: {
+		name: "isoMorph",
+		namespace: "https://github.com/scarf005",
+		description: "use the superior time unit",
+		"description:kr": "웹페이지 날짜 형식을 ISO 8601로 변경합니다.",
+		version: "1.0.0",
+		icon: new URL("https://www.google.com/s2/favicons?sz=64&domain=github.com"),
+		homepageURL: new URL("https://github.com/scarf005/userscript"),
+		supportURL: new URL(
+			"https://github.com/scarf005/userscript/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc",
+		),
+		downloadURL: new URL(
+			"https://raw.githubusercontent.com/scarf005/userscript/main/dist/isomorph.user.js",
+		),
+	},
+	resources: {
+		match: ["*://*/*"],
+	},
+})
+
+if (import.meta.main) {
+	const code = await bundleUserScript({ url: import.meta.resolve("./mod.ts"), metadata })
+	await Deno.writeTextFile(resolve("../../dist/isomorph.user.js"), code)
+}
---
+++ packages/isomorph/mod.ts
@@ -0,0 +1,72 @@
+const toISODate: (s: Date) => string = (s): string => s.toISOString().split("T")[0]
+
+const toISODateTime: (s: Date) => string = (s): string =>
+	s.toISOString().split(".")[0].replace("T", " ")
+
+const replaceText: (el: HTMLElement, s: string) => void = (el, s): void =>
+	Array.from(el.childNodes)
+		.filter((node) => node.nodeType === Node.TEXT_NODE)
+		.forEach((node) => node.textContent = s)
+
+const monthname: Record<string, string> = {
+	"January": "01",
+	"February": "02",
+	"March": "03",
+	"April": "04",
+	"May": "05",
+	"June": "06",
+	"July": "07",
+	"August": "08",
+	"September": "09",
+	"October": "10",
+	"November": "11",
+	"December": "12",
+}
+
+/**
+ * `30 January 2023 @ 19:06 UTC` -> `2023-01-30T19:06:00.000Z`
+ */
+const parseItch: (s: string) => Date = (s): Date => {
+	const [date, time] = s.split(" @ ")
+	const [day, month, year] = date.split(" ")
+	const [hour, minute] = time.replace(" UTC", "").split(":")
+
+	return new Date(`${year}-${monthname[month]}-${day}T${hour}:${minute}:00.000Z`)
+}
+
+/**
+ * @param xs - target elements
+ * @param getTime - query the time from the element
+ * @param fn - modify the element
+ */
+const isoMorph: ({ xs, fn }: {
+	xs: NodeListOf<HTMLElement>
+	getTime: (x: HTMLElement) => Date
+	fn?: (x: HTMLElement, time: Date) => void
+}) => void = ({ getTime, xs, fn = (el, time) => void (el.innerText = toISODate(time)) }) =>
+	Array.from(xs)
+		.forEach((el) => {
+			const datetime = getTime(el)
+			fn(el, datetime)
+			el.setAttribute("data-isomorph", "true")
+		})
+
+isoMorph({
+	xs: document.querySelectorAll("time"),
+	getTime: (el) => new Date(el.getAttribute("datetime") || el.innerText),
+})
+
+/**
+ * `<abbr title="30 January 2023 @ 19:06 UTC"><span class="icon icon-stopwatch" aria-hidden="true"></span> Jan 30, 2023</abbr>`
+ */
+if (location.hostname.endsWith("itch.io")) {
+	isoMorph({
+		xs: document.querySelectorAll("abbr[title]"),
+		getTime: (el) => parseItch(el.getAttribute("title")!),
+		fn: (el, time) => {
+			console.log(el)
+			el.setAttribute("title", toISODateTime(time))
+			replaceText(el, toISODate(time))
+		},
+	})
+}
